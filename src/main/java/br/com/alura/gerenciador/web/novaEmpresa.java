4ef25e8a44bdc2e80546bab4a9acf6ba6e7b6c99
Iago George <iagogeorge20@gmail.com>
2018-02-25 17:55:49 -0300

    primeira versao

---
+++ src/main/java/br/com/alura/gerenciador/web/novaEmpresa.java
@@ -0,0 +1,38 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.Empresa;
+import br.com.alura.gerenciador.dao.EmpresaDAO;
+
+
+@WebServlet(urlPatterns="/novaEmpresa")
+public class novaEmpresa extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	
+		String nome = req.getParameter("nome");
+		Empresa empresa = new Empresa(nome);
+		
+		EmpresaDAO empresaDao = new EmpresaDAO();
+		empresaDao.adiciona(empresa);
+		
+		PrintWriter writer = resp.getWriter();
+		writer.print("<html><body>Empresa Adicionada:"+ nome +"</body></html>");
+	
+//		RequestDispatcher rd = req.getRequestDispatcher("/busca");
+//        rd.forward(req, resp);
+	}
+	
+	
+}
